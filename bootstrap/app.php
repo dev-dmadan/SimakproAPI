e59f973e760d37bb2cfdce165a8ee2b902ef3fcb
Romadan Saputra <42397721+dev-dmadan@users.noreply.github.com>
2020-07-15 01:18:48 +0700

    setup pusher

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -25,7 +25,7 @@ $app = new Laravel\Lumen\Application(
 
 $app->withFacades();
 
-// $app->withEloquent();
+$app->withEloquent();
 
 /*
 |--------------------------------------------------------------------------
@@ -60,6 +60,7 @@ $app->singleton(
 */
 
 $app->configure('app');
+$app->configure('broadcasting');
 
 /*
 |--------------------------------------------------------------------------
@@ -93,7 +94,8 @@ $app->configure('app');
 
 // $app->register(App\Providers\AppServiceProvider::class);
 // $app->register(App\Providers\AuthServiceProvider::class);
-// $app->register(App\Providers\EventServiceProvider::class);
+$app->register(App\Providers\EventServiceProvider::class);
+$app->register(\Illuminate\Broadcasting\BroadcastServiceProvider::class);;
 
 /*
 |--------------------------------------------------------------------------
